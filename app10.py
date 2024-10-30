08d3c1e4e2af80b50a92801b3047c9057c5488f7
jhh0707 <wonderwall610@gmail.com>
2024-10-30 12:01:17 +0900

    Add files via upload

--- app10.py
+++ app10.py
@@ -32,3 +32,29 @@ answers = [
 question_embeddings = encoder.encode(questions)
 df = pd.DataFrame({'question': questions, '챗봇': answers, 'embedding': list(question_embeddings)})
 df
+
+
+#유저의 질문을 입력받아 가장 유사한 질문의 답변을 반환하는 함수를 작성합니다.
+def greet(user, history=[]):
+    #사용자 입력 임베딩(숫자로 변환)
+    embedding = encoder.encode(user)
+
+    #유사도 계산하여 가장 유사한 응답 찾기
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['distance'].idxmax()]
+
+    #히스토리에 추가하여 대화 유지
+    history.append([user, answer['챗봇']])
+    return history, history
+
+#Gradio의 인터페이스를 통해 greet 함수를 대화형으로 사용할 수 있도록 설정합니다.
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "state"],
+    outputs=["chatbot", "state"],
+    title="타격폼의 주인공 찾기",
+    description="프로젝트에 관한 질문을 입력해보세요. 예: 포트폴리오 주제가 뭔가요?"
+)
+
+# 챗봇 실행
+demo.launch(debug=True, share=True)
